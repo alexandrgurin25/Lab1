a00f55cc1fc80d4319aa78283a1e13daaf4d4c84
alex <alexandrgurin04@gmail.com>
2023-09-10 13:36:38 +0300

    task2AllFile

---
+++ src/ru/mirea/lab2/TestAuthor.java
@@ -0,0 +1,18 @@
+package ru.mirea.lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Иванов Иван", "ivan@email.ru", 'M');
+
+        //System.out.println(author);
+        System.out.println("Имя автора " + author.getName());
+        System.out.println("Email автора " + author.getEmail());
+
+        author.setEmail("Ivabuskla@email.ru");
+        System.out.println("Новый Email " + author.getEmail());
+
+        System.out.println("Пол автора: " + author.getGender());
+        System.out.println(author.toString());
+
+    }
+}
