2ba4043d0f5945de0b5361a09b411719458086aa
alex <alexandrgurin04@gmail.com>
2023-11-09 22:06:22 +0300

    LinkedList1

---
+++ src/ru/mirea/taskList1/Handmade/task1/Student.java
@@ -0,0 +1,24 @@
+package ru.mirea.taskList1.Handmade.task1;
+
+import java.util.Scanner;
+
+public class Student {
+    public String name;
+    public int age;
+    public int height;
+
+    public Student(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+}
---
+++ src/ru/mirea/taskList1/Library/task1/Student.java
@@ -0,0 +1,31 @@
+package ru.mirea.taskList1.Library.task1;
+
+import java.util.Scanner;
+
+public class Student {
+    String name;
+    int age;
+    int height;
+
+    public Student(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void readAttributesFromConsole(){
+        Scanner scanner = new Scanner(System.in);
+        this.name = scanner.nextLine();
+        this.age = scanner.nextInt();
+        this.height = scanner.nextInt();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+}
